a0c251059a87c59db7a802d0b50233987368e4d0
Zain_ikram_nawaz <zainikram704@gmail.com>
2025-11-22 18:35:30 +0500

    a

--- BBS_POS/start.sh
+++ BBS_POS/start.sh
@@ -2,6 +2,5 @@
 # Build the project
 dotnet publish BBS_POS/BBS_POS.csproj -c Release -o ./publish
 
-# Run the published application
-# Replace 'BBS_POS.dll' with your actual DLL name
+
 dotnet ./publish/BBS_POS.dll
